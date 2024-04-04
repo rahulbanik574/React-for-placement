8934643ca1d17523873d6b08c8f227f307720d10
rahulbanik574 <rahulbanik574@gmial.com>
2024-04-05 01:26:11 +0530

    updated

---
+++ 05_password_generator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ const [length,setLength] = useState(8);
+ const [numberAllowed,setNumberAllowed]= useState(false);
+
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>PaSSWORD gENERATOR</h1>
+    </>
+  )
+}
+
+export default App
